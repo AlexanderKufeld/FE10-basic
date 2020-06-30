3b504ab37f2a184585ecf74938a52dba3d9e2a21
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-06-30 18:47:16 +0200

    solution with randomnumbers

--- paragraphs/script.js
+++ paragraphs/script.js
@@ -1,4 +1,4 @@
-const colors = ['white', 'red', 'green', 'blue'];
+/*const colors = ['white', 'red', 'green', 'blue'];
 //                 0       1       2        3  
 
 let currentColor = 0;
@@ -16,5 +16,23 @@ function changeColor(param) {
         currentColor++;
         return param.style['background-color'] = colors[currentColor % colors.length];
     }
-}
+}*/
 
+/*let rNumbers = document.getElementsByTagName('button');
+for (let i = 0; i < rNumbers.length; i++) {
+   rNumbers[i].addEventListener('click', function Random() {
+      const max = 100;
+      return function () {
+         const uniqe = new Set();
+         while (uniqe.size !== max) {
+            uniqe.add(Math.floor(Math.random() * max) + 1);
+         }
+         console.log('Element # ' + x + ' with random number - ' + [...uniqe]);  
+      }
+   }(i));
+}*/
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
