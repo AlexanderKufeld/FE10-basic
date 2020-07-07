e50d5be099c725050149b1b431a76ebe7714fdb4
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-07-07 11:36:22 +0200

    solution for feth weather with json

---
+++ fetch_weather/script.js
@@ -0,0 +1,14 @@
+
+function getWeather() {
+    let checkCountry = document.getElementById('country').value;
+    let checkCity = document.getElementById('city').value;
+    let appID = 'bed156d40174100c73406417523ddaf1';
+        fetch('http://api.openweathermap.org/data/2.5/weather?country='+checkCountry+'&q='+checkCity+'&APPID='+appID)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            document.querySelector('table').innerHTML = 'Temperature in ' + checkCity + ' is ' + data.main.temp +' fahrenheit' + '<br>' + 'Wind speed: ' + data.wind.speed + ' m/s'
+        })
+    }
+    
